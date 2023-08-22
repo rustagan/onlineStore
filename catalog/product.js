7b1eb031a7a9e8275843ed0dff0570abd8a172c3
rustagan <rustem.azeev@gmail.com>
2023-08-23 00:22:59 +0300

    upd

--- catalog/product.js
+++ catalog/product.js
@@ -2,50 +2,50 @@ const CATALOGY = [
     {
         id: 'el1',
         name: 'Сплиттер передний',
-        imgSrc: 'img/splitter.jpg',
-        imgSrc2: 'img/splitter2.jpg',
+        imgSrc: '/onlineStore/img/splitter.jpg',
+        imgSrc2: '/onlineStore/img/splitter2.jpg',
         price: '190'
     },
     {
         id: 'el2',
         name: 'Дактейл',
-        imgSrc: 'img/ducktail.jpg',
-        imgSrc2: 'img/ducktail2.jpg',
+        imgSrc: '/onlineStore/img/ducktail.jpg',
+        imgSrc2: '/onlineStore/img/ducktail2.jpg',
         price: '120'
     },
     {
         id: 'el3',
         name: 'Диффузор задний',
-        imgSrc: 'img/diffuser.jpg',
-        imgSrc2: 'img/diffuser2.jpg',
+        imgSrc: '/onlineStore/img/diffuser.jpg',
+        imgSrc2: '/onlineStore/img/diffuser2.jpg',
         price: '250'
     },
     {
         id: 'el4',
         name: 'Элероны',
-        imgSrc: 'img/elerons.jpg',
-        imgSrc2: 'img/elerons2.jpg',
+        imgSrc: '/onlineStore/img/elerons.jpg',
+        imgSrc2: '/onlineStore/img/elerons2.jpg',
         price: '80'
     },
     {
         id: 'el5',
         name: 'Элероны',
-        imgSrc: 'img/elerons.jpg',
-        imgSrc2: 'img/elerons2.jpg',
+        imgSrc: '/onlineStore/img/elerons.jpg',
+        imgSrc2: '/onlineStore/img/elerons2.jpg',
         price: '80'
     },
     {
         id: 'el6',
         name: 'Элероны',
-        imgSrc: 'img/elerons.jpg',
-        imgSrc2: 'img/elerons2.jpg',
+        imgSrc: '/onlineStore/img/elerons.jpg',
+        imgSrc2: '/onlineStore/img/elerons2.jpg',
         price: '80'
     },
     {
         id: 'el7',
         name: 'Элероны',
-        imgSrc: 'img/elerons.jpg',
-        imgSrc2: 'img/elerons2.jpg',
+        imgSrc: '/onlineStore/img/elerons.jpg',
+        imgSrc2: '/onlineStore/img/elerons2.jpg',
         price: '80'
     }
 ];
